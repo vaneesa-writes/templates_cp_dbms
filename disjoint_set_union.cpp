c675beb2048a05838f6569c3e255a955c989b0d3
vaneesa-writes <96366365+vaneesa-writes@users.noreply.github.com>
2022-09-26 10:11:27 +0530

    Update disjoint_set_union.cpp

--- disjoint_set_union.cpp
+++ disjoint_set_union.cpp
@@ -3,8 +3,6 @@ class DisjSet {
  
 public:
  
-	// Constructor to create and
-	// initialize sets of n items
 	DisjSet(long long int n)
 	{
 		rank = new long long int[n];
@@ -13,7 +11,6 @@ public:
 		makeSet();
 	}
  
-	// Creates n single item sets
 	
 	int count_comp(){
 	    long long int ans=0;
@@ -30,41 +27,25 @@ public:
 		}
 	}
  
-	// Finds set of given item x
 	int find(int x)
 	{
-		// Finds the representative of the set
-		// that x is an element of
 		if (parent[x] != x) {
  
-			// if x is not the parent of itself
-			// Then x is not the representative of
-			// his set,
 			parent[x] = find(parent[x]);
  
-			// so we recursively call Find on its parent
-			// and move i's node directly under the
-			// representative of this set
 		}
  
 		return parent[x];
 	}
  
-	// Do union of two sets represented
-	// by x and y.
 	void Union(int x, int y)
 	{
-		// Find current sets of x and y
 		long long int xset = find(x);
 		long long int yset = find(y);
  
-		// If they are already in same set
 		if (xset == yset)
 			return;
  
-		// Put smaller ranked item under
-		// bigger ranked item if ranks are
-		// different
 		if (rank[xset] < rank[yset]) {
 			parent[xset] = yset;
 		}
@@ -72,11 +53,11 @@ public:
 			parent[yset] = xset;
 		}
  
-		// If ranks are same, then increment
-		// rank.
 		else {
 			parent[yset] = xset;
 			rank[xset] = rank[xset] + 1;
 		}
 	}
 };
+
+
